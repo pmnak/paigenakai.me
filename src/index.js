534ff6c4bb29e3f1543d49856c4a8901ba0bf300
Paige Nakai <86543892+pmnak@users.noreply.github.com>
2024-04-10 12:07:17 -0400

    all files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+$('#main').html('Here we go!');
+
+let num = 0;
+
+setInterval(() => {
+  num += 1;
+  document.querySelector('#main').textContent = `You've been on this page for ${num} seconds.`;
+}, 1000);
